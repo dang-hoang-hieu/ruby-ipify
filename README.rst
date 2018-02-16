4a5e24a6677873899ea1f418f7dfebc5fc4825e1
Hieu Dang <hoanghieu3690@gmail.com>
2018-02-16 11:59:49 +0900

    generate gem by bundler

--- README.rst
+++
@@ -1,3 +0,0 @@
-# ruby-ipify
-
-This gem is for retrieving public ip address from ipify's API service 
---
+++ spec/ipify_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Ipify do
+  it "has a version number" do
+    expect(Ipify::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
