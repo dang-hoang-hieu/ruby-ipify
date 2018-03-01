a95b1acfaa4a41629dddb407546a2792bfeab587
Hieu Dang <hoanghieu3690@gmail.com>
2018-03-01 16:09:31 +0900

    handle exception

--- lib/ipify.rb
+++ lib/ipify.rb
@@ -4,9 +4,18 @@ require 'net/http'
 API_URI = 'https://api.ipify.org'
 
 module Ipify
+  ConnectionError = Class.new(StandardError)
+  ServiceError = Class.new(StandardError)
+
+  # @return [String] this machine's public ip
   def self.ip format = nil, callback = nil
     res = Net::HTTP.get_response(build_request(format, callback))
+
+    raise ServiceError, "Received an invalid status code from ipify: #{res.status_code}. The service might be experiencing issues." if '200' !== res.code
+
     res.body
+  rescue Net::OpenTimeout
+    raise ConnectionError, "The request failed because it wasn't able to reach the ipify service. This is most likely due to a networking error of some sort."
   end
 
   private
