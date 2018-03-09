263744f24e1e1b6136d74f553f1e9ea7e778b7e1
Hieu Dang <hoanghieu3690@gmail.com>
2018-03-09 15:20:54 +0900

    add exponential backoff

--- lib/ipify.rb
+++ lib/ipify.rb
@@ -2,6 +2,7 @@ require "ipify/version"
 require 'net/http'
 
 API_URI = 'https://api.ipify.org'
+MAX_RETRIES = 3
 
 module Ipify
   ConnectionError = Class.new(StandardError)
@@ -11,11 +12,18 @@ module Ipify
   def self.ip format = nil, callback = nil
     res = Net::HTTP.get_response(build_request(format, callback))
 
-    raise ServiceError, "Received an invalid status code from ipify: #{res.status_code}. The service might be experiencing issues." if '200' !== res.code
+    raise ServiceError, "Received an invalid status code from ipify: #{res.status_code}. The service might be experiencing issues." if '200' != res.code
 
     res.body
   rescue Net::OpenTimeout
-    raise ConnectionError, "The request failed because it wasn't able to reach the ipify service. This is most likely due to a networking error of some sort."
+    retries ||= 0
+    if retries < MAX_RETRIES
+      retries += 1
+      sleep 2 ** retries
+      retry
+    else
+      raise ConnectionError, "The request failed because it wasn't able to reach the ipify service. This is most likely due to a networking error of some sort."
+    end
   end
 
   private
